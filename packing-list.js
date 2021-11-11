87716d6a0000b93726bb40e79beb5724c1ebfbfd
Archana Deulkar <88867952+sawantarchna@users.noreply.github.com>
2021-11-11 15:59:57 -0500

    added While Loop

--- packing-list.js
+++ packing-list.js
@@ -1,7 +1,18 @@
-const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+//Iterating Over an Array With for
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 } 
 packingList[3];
+
+/********************************************************** */
+
+//Iterating Over an Array With while
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
